6cf6e240b68beac586c7be12802eabafb845854c
Luiz Repiso <luizrepiso@yahoo.es>
2022-03-16 19:22:03 -0300

    ButtonProduct

--- src/components/ProductCard/ButtonProduct/index.tsx
+++ src/components/ProductCard/ButtonProduct/index.tsx
@@ -5,5 +5,8 @@ import './styles.css';
 const ButtonProduct = () => {
   return (
     <button className="btn btn-primary btn-icon-product">
+      <p>Comprar</p>
     </button>
+  );
 }
+export default ButtonProduct;
